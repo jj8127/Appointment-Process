4d7fce7c12d7d9c96ab3e4caf96ca28847db2301
jj8127 <jj81271000@gmail.com>
2026-02-11 16:50:24 +0900

    chore(test): ignore web build artifacts in jest scan

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  modulePathIgnorePatterns: [
+    '<rootDir>/web/.next/',
+    '<rootDir>/web/out/',
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/web/.next/',
+  ],
+  watchPathIgnorePatterns: [
+    '<rootDir>/web/.next/',
+    '<rootDir>/web/out/',
+  ],
   testMatch: [
     '**/__tests__/**/*.test.[jt]s?(x)',
     '**/?(*.)+(spec|test).[jt]s?(x)'
